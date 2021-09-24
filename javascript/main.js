c4c7d70b9c934c299866d990c63bf1ea64275ce9
Jeet <jeetpatel0070070@gmail.com>
2021-09-24 15:26:23 +0530

    add new content

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,21 @@
+// import resorceses from node-modules
 import { gsap, ScrollTrigger, Draggable, MotionPathPlugin } from "gsap/all";
+import Parallax from "parallax-js";
+import LocomotiveScroll from "locomotive-scroll";
+
+// smooth scroll (locomotive-scroll init)
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+});
+
+// nav flexibility
+const nav = document.getElementById("nav");
+
+window.addEventListener("scroll", function () {
+  if (this.pageYOffset > 100) {
+    nav.classList.add("active");
+  } else {
+    nav.classList.remove("active");
+  }
+});
