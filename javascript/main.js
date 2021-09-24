d9cd4e354c18e15b26e3eb258870d4d6f0e5051b
Jeet <jeetpatel0070070@gmail.com>
2021-09-24 19:28:15 +0530

    add new file

--- javascript/main.js
+++ javascript/main.js
@@ -1,21 +1,32 @@
 // import resorceses from node-modules
 import { gsap, ScrollTrigger, Draggable, MotionPathPlugin } from "gsap/all";
 import Parallax from "parallax-js";
-import LocomotiveScroll from "locomotive-scroll";
-
-// smooth scroll (locomotive-scroll init)
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("[data-scroll-container]"),
-  smooth: true,
-});
+// import * as THREE from "three";
+// import hoverEffect from "hover-effect";
 
 // nav flexibility
 const nav = document.getElementById("nav");
 
 window.addEventListener("scroll", function () {
-  if (this.pageYOffset > 100) {
+  if (window.pageYOffset > 100) {
     nav.classList.add("active");
   } else {
     nav.classList.remove("active");
   }
 });
+
+// parallax effect in banner
+
+var scene = document.getElementById("scene");
+var scene_two = document.getElementById("scene-2");
+var scene_three = document.getElementById("scene-3");
+var scene_four = document.getElementById("scene-4");
+var scene_five = document.getElementById("scene-5");
+var scene_six = document.getElementById("scene-6");
+var parallaxInstance = new Parallax(scene);
+var parallaxInstance_two = new Parallax(scene_two);
+var parallaxInstance_three = new Parallax(scene_three);
+var parallaxInstance_four = new Parallax(scene_four);
+var parallaxInstance_five = new Parallax(scene_five);
+var parallaxInstance_six = new Parallax(scene_six);
+// about section picture hover effect
